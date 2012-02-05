44b583996dd3f7800a9bd4ab92b024b133ca4f47
Jeff Wardian <krinsdeath@hotmail.com>
2012-02-04 23:41:01 -0800

    Added teleport.tp.other to /tp;

--- src/main/java/net/krinsoft/teleportsuite/commands/TPCommand.java
+++ src/main/java/net/krinsoft/teleportsuite/commands/TPCommand.java
@@ -3,6 +3,7 @@ package net.krinsoft.teleportsuite.commands;
 import net.krinsoft.teleportsuite.Request;
 import net.krinsoft.teleportsuite.TeleportSuite;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.permissions.PermissionDefault;
 
 import java.util.List;
@@ -25,9 +26,19 @@ public class TPCommand extends TeleportCommand {
     }
 
     public void runCommand(CommandSender sender, List<String> args) {
+        if (sender instanceof ConsoleCommandSender && args.size() < 2) {
+            sender.sendMessage(plugin.getLocalization(null).get("error.invalid.arguments", null));
+            return;
+        }
         CommandSender from = sender;
         String to = args.get(0);
         if (args.size() == 2 && plugin.getServer().getPlayer(args.get(0)) != null) {
+            if (!sender.hasPermission("teleport.tp.other")) {
+                if (!(sender instanceof ConsoleCommandSender)) {
+                    plugin.getManager().getPlayer(sender.getName()).sendLocalizedString("error.permission", "teleport.tp.other");
+                    return;
+                }
+            }
             from = plugin.getServer().getPlayer(args.get(0));
             to = args.get(1);
         }
