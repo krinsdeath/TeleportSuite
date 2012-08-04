6486906edc3b74893c7b946ea3439f184838e9c0
Jeffrey Wardian <krinsdeath@hotmail.com>
2012-08-03 22:03:25 -0700

    Updated to 1.3.1-R0.1-SNAPSHOT and AsyncPlayerChatEvent.

--- src/main/java/net/krinsoft/teleportsuite/listeners/PlayerListener.java
+++ src/main/java/net/krinsoft/teleportsuite/listeners/PlayerListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -41,7 +42,7 @@ public class PlayerListener implements Listener {
     }
 
     @EventHandler
-    void playerChat(PlayerChatEvent event) {
+    void playerChat(AsyncPlayerChatEvent event) {
         if (event.isCancelled()) { return; }
         if (event.getPlayer() == null) { return; }
         Player p = event.getPlayer();
